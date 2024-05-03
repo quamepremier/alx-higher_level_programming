5941520e54a84c692ad51127f7c9d4729d425a73
quamepremier <ericsarpong92@gmail.com>
2024-05-03 08:18:44 -0700

    Java_web_queries

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('#btn_translate').click(translateHello);
+  $('#language_code').keypress(function(e) {
+    if (e.which == 13) {
+      translateHello();
+    }
+  });
+
+  function translateHello() {
+    let languageCode = $('#language_code').val();
+    $.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`, function(data) {
+      $('#hello').text(data.hello);
+    });
+  }
+});
